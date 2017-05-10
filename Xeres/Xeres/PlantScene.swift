46bf28b09bd2c81b2c8d5c82c351ff027e59fe25
Johan Wieslander <jwie@kth.se>
2017-05-10 12:53:36 +0200

    MotionManager now gives gyro input

--- Xeres/Xeres/PlantScene.swift
+++ Xeres/Xeres/PlantScene.swift
@@ -5,6 +5,7 @@
 //  Copyright © 2017 wiemor. All rights reserved.
 
 import Foundation
+import CoreMotion
 import SpriteKit
 
 class PlantScene: SKScene {
@@ -13,11 +14,31 @@ class PlantScene: SKScene {
     
     var tree: Tree!
     
+    var sun: Sun!
+    
+    var motionManager = CMMotionManager()
+    var accumulatedRotation = 0.0
+    
     override init(size: CGSize) {
         super.init(size: size)
-        
+    }
+    
+    override func didMove(to view: SKView) {
         PlantScene.scene = self
-        tree = Tree(scene: self)
+        //self.tree = Tree(scene: self)
+        
+        self.sun = Sun(position: CGPoint(x: frame.midX, y: frame.midY),
+                       minWidth: frame.minX,
+                       maxWidth: frame.maxX)
+        addChild(sun)
+        
+        motionManager.startGyroUpdates(to: OperationQueue.current!, withHandler: motionUpdate)
+    }
+    
+    func motionUpdate(gyroData: CMGyroData?, error: Error?) {
+        if let rotationRate = gyroData?.rotationRate {
+            accumulatedRotation += rotationRate.x
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -25,10 +46,13 @@ class PlantScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
-        self.removeAllChildren()
+        //self.removeAllChildren()
         
-        tree.grow(from: CGPoint(x: frame.midX, y: frame.minY))
+        //tree.grow(from: CGPoint(x: frame.midX, y: frame.minY))
         
-        tree.update()
+        //tree.update()
+        
+        sun.move(force: CGFloat(accumulatedRotation))
+        accumulatedRotation = 0
     }
 }
