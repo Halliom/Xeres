b1a628b7260292ec27ffb82fc3f063b31f98beff
Johan Wieslander <jwie@kth.se>
2017-05-11 16:20:41 +0200

    Temporary fix so leafs now render, closes #14

--- Xeres/Xeres/Leaf.swift
+++ Xeres/Xeres/Leaf.swift
@@ -21,7 +21,7 @@ class Leaf: SKNode {
         super.init()
         self.position = offset
         
-        createPath(in: CGRect(x: 0, y: 0, width: 100, height: 100),
+        createPath(in: CGRect(x: 0, y: 0, width: 5, height: 5),
                    from: CGPoint(), inDirection: atan(direction.y / direction.x))
         shapeNode.path = shape.cgPath
         shapeNode.fillColor = UIColor(displayP3Red: 0.0, green: 150 / 255, blue: 50 / 255, alpha: 1.0)
@@ -32,72 +32,68 @@ class Leaf: SKNode {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // Creates a leaf node fo
+    // Creates a path to render the leaf ndoe
     // Angles goes clockwise as we're dealing with a left handed (?) coordinate system
     // Comment: The rotation and scale should be turned into CGAffineTransform matrices
     // to shorten the code vastly!
     func createPath(in bound: CGRect, from start: CGPoint, inDirection angle: CGFloat) {
-        
         // This should be turned into something more flexible
         var from = CGPoint(x: 0,   y: 0)
-        var to   = CGPoint(x: 1,   y: 1)
-        var ctrl = CGPoint(x: 4/5, y: 1)
+        var to   = CGPoint(x: bound.maxX,   y: bound.maxY)
+        var ctrl = CGPoint(x: (3 / 5) * bound.maxX, y: bound.maxY)
         
         // Sets angle=0 to mean "to the right"
-        let rotationAngle = angle - CGFloat.pi/4
+        let rotationAngle = angle - CGFloat.pi / 4
         
         // Put the leaf into position, to calculate scale factors
-        from = rotateAroundOrigin(from, withAngle: rotationAngle)
-        to   = rotateAroundOrigin(to, withAngle: rotationAngle)
-        ctrl = rotateAroundOrigin(ctrl, withAngle: rotationAngle)
 
-        shape.move(to: from)
+        shape.move(to: CGPoint())
         shape.addQuadCurve(to: to, controlPoint: ctrl)
         shape.close()
         
-        let translate = CGAffineTransform(translationX: start.x, y: start.y)
-        shape.apply(translate)
-        
-        // Calculate scale factors depending on draw direction from 'start'
-        
-        let box = shape.bounds
-        
-        let scaleFactor : CGFloat
-        let scaleFactX  : CGFloat
-        let scaleFactY  : CGFloat
-    
-        
-        if box.maxX - 1 < start.x { // to the left
-            scaleFactX  = box.maxX  / box.width
-            
-            if box.maxY - 1 < start.y { // up
-                scaleFactY  = box.maxY / box.height
-            } else { // down
-                scaleFactY  = (bound.height-box.maxY) / box.height
-            }
-            
-        } else { // to the right
-            scaleFactX  = (bound.width-box.maxX)  / box.width
-            
-            if box.maxY - 1 < start.y { // up
-                scaleFactY  = box.maxY / box.height
-            } else { // down
-                scaleFactY  = (bound.height-box.maxY) / box.height
-            }
-        }
-        
-        scaleFactor = min(scaleFactX, scaleFactY)
-        
-        // Remake the shape of the leaf with correct scale
-        from = scale(from, from: from, by: scaleFactor)
-        to   = scale(to, from: from, by: scaleFactor)
-        ctrl = scale(ctrl, from: from, by: scaleFactor)
-        
-        shape.removeAllPoints()
-        shape.move(to: from)
-        shape.addQuadCurve(to: to, controlPoint: ctrl)
-        shape.close()
-        
-        shape.apply(translate)
+        shape.apply(CGAffineTransform(rotationAngle: rotationAngle))
+//        let translate = CGAffineTransform(translationX: start.x, y: start.y)
+//        shape.apply(translate)
+//        
+//        // Calculate scale factors depending on draw direction from 'start'
+//        
+//        let box = shape.bounds
+//        
+//        let scaleFactor: CGFloat
+//        let scaleFactX: CGFloat
+//        let scaleFactY: CGFloat
+//        
+//        if box.maxX - 1 < start.x { // to the left
+//            scaleFactX  = box.maxX  / box.width
+//            
+//            if box.maxY - 1 < start.y { // up
+//                scaleFactY  = box.maxY / box.height
+//            } else { // down
+//                scaleFactY  = (bound.height-box.maxY) / box.height
+//            }
+//            
+//        } else { // to the right
+//            scaleFactX  = (bound.width-box.maxX)  / box.width
+//            
+//            if box.maxY - 1 < start.y { // up
+//                scaleFactY  = box.maxY / box.height
+//            } else { // down
+//                scaleFactY  = (bound.height-box.maxY) / box.height
+//            }
+//        }
+//        
+//        scaleFactor = min(scaleFactX, scaleFactY)
+//        
+//        // Remake the shape of the leaf with correct scale
+//        from = scale(from, from: from, by: scaleFactor)
+//        to   = scale(to, from: from, by: scaleFactor)
+//        ctrl = scale(ctrl, from: from, by: scaleFactor)
+//        
+//        shape.removeAllPoints()
+//        shape.move(to: from)
+//        shape.addQuadCurve(to: to, controlPoint: ctrl)
+//        shape.close()
+//        
+//        shape.apply(translate)
     }
 }
